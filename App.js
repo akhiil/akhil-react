10d3c58f643a584b914fa285b5a88ae5a4e86b08
Akhil Kumar Anand <52624544+akhiil@users.noreply.github.com>
2021-01-13 03:06:43 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import phone from "./screens/phone.js";
+import otp from "./screens/otp.js";
+import main from "./screens/main";
+import Header from "./components/header";
+
+const AppRouter =()=>{
+    return(
+       <BrowserRouter>
+       <Header />
+       <Switch>
+       <Route path="/" component={phone} exact/>
+           <Route path="/otp" component={otp} exact/>
+       </Switch>
+       </BrowserRouter>
+    )
+}
+export default AppRouter;
